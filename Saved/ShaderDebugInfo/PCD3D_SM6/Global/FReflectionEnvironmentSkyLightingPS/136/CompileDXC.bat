d08b9ba0a63f1b93aa5e39531605f623a79b5084
SupremeGodOfDesign <122851709+SupremeGodOfDesign@users.noreply.github.com>
2023-12-23 19:47:46 -0300

    Assets

---
+++ Saved/ShaderDebugInfo/PCD3D_SM6/Global/FReflectionEnvironmentSkyLightingPS/136/CompileDXC.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET DXC="D:\Programas\Untral engine\UE_5.3\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
+IF NOT EXIST %DXC% (
+	ECHO Couldn't find dxc.exe under "D:\Programas\Untral engine\UE_5.3\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
+	GOTO :END
+)
+%DXC% -HV 2021 -Zpr -O1 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T ps_6_6 -E ReflectionEnvironmentSkyLighting -Fc ReflectionEnvironmentPixelShader.d3dasm -Fo ReflectionEnvironmentPixelShader.dxil ReflectionEnvironmentPixelShader
+:END
+PAUSE
---
+++ Saved/ShaderDebugInfo/PCD3D_SM6/Global/FReflectionTraceVoxelsCS/65/CompileDXC.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET DXC="D:\Programas\Untral engine\UE_5.3\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
+IF NOT EXIST %DXC% (
+	ECHO Couldn't find dxc.exe under "D:\Programas\Untral engine\UE_5.3\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
+	GOTO :END
+)
+%DXC% -HV 2021 -Zpr -O3 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T cs_6_6 -E ReflectionTraceVoxelsCS -Fc LumenReflectionTracing.d3dasm -Fo LumenReflectionTracing.dxil LumenReflectionTracing
+:END
+PAUSE
